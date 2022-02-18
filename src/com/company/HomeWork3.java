375fce76caeb8586c35e6ffd115bd9a67e21b977
alina <alina.pdlsn@gmail.com>
2022-02-18 21:19:17 +0200

    update

--- src/com/company/HomeWork3.java
+++ src/com/company/HomeWork3.java
@@ -3,6 +3,12 @@ package com.company;
 public class HomeWork3 {
 
     public static void main(String[] args) {
+
+        exerciseOne();
+        exerciceTwo();
+    }
+
+    public static void exerciseOne() {
         byte byteValue = 127;
         short shortValue = 32;
         int intValue = 28;
@@ -13,7 +19,7 @@ public class HomeWork3 {
 
         System.out.println();
         System.out.println("Задача №1");
-        System.out.println("Значение типа Byte = " + byteValue + ";");
+        System.out.println("Значение типа byte = " + byteValue + ";");
         System.out.println("Значение типа short = " + shortValue + ";");
         System.out.println("Значение типа int = " + intValue + ";");
         System.out.println("Значение типа long = " + longValue + ";");
@@ -21,17 +27,25 @@ public class HomeWork3 {
         System.out.println("Значение типа double = " + doubleValue + ";");
         System.out.println("Значение типа float = " + floatValue + ".");
 
+    }
+
+    public static void exerciceTwo() {
+        Square sqr = new Square();
+        sqr.size = 4;
+        sqr.area = sqr.size * sqr.size;
+        sqr.perimetr = sqr.size * 4;
+        sqr.compare = sqr.area == sqr.perimetr;
+
+        Square sqr2 = new Square();
+        sqr2.size = 2;
+        sqr2.area = sqr2.size * sqr2.size;
+        sqr2.perimetr = sqr2.size * 4;
+        sqr2.compare = sqr2.area == sqr2.perimetr;
+
         System.out.println();
         System.out.println("Задача №2");
-        show(4);
-        show(2);
-
+        System.out.println("Периметр равен площади: " + sqr.compare);
+        System.out.println("Периметр равен площади: " + sqr2.compare);
     }
 
-    public static void show(int val) {
-        Square sqr = new Square();
-        sqr.processSquareData(val);
-        System.out.println("Периметр равен площади:" + sqr.comparison);
-
-    }
 }
--- src/com/company/Square.java
+++ src/com/company/Square.java
@@ -1,20 +1,9 @@
 package com.company;
 
 public class Square {
-    int perimeter;
     int area;
-    boolean comparison;
-
-    public void processSquareData(int value) {
-        this.perimeter = value * 4;
-        this.area = value * value;
-
-        if (this.perimeter == this.area) {
-            this.comparison = true;
-        } else {
-            this.comparison = false;
-        }
-
-    }
+    int perimetr;
+    boolean compare;
+    int size;
 
 }
